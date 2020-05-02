1e57581dfd6a84aa0d08ae7c859010aaf834f486
williamkpchan <williamkpchan@gmail.com>
2020-05-02 10:52:08 +0800

    update

--- showAllCharts.js
+++ showAllCharts.js
@@ -14,7 +14,7 @@ thisImgPCode= "&period=7";
 
 function showAllCharts(){
 //    $("#codelist").load("HKCodelist.txt");
-  thisImgHead = "<img src='http://charts.aastocks.com/servlet/Charts?fontsize=12&15MinDelay=F&lang=1&titlestyle=1&vol=1&Indicator=3&indpara1=3&indpara2=6&indpara3=9&indpara4=12&indpara5=15&subChart2=3&ref2para1=12&ref2para2=26&ref2para3=9&subChart3=12&ref3para1=0&ref3para2=0&ref3para3=0&scheme=3&com=100&chartwidth=680&chartheight=400&stockid=";
+  thisImgHead = "<img class='lazy' data-src='http://charts.aastocks.com/servlet/Charts?fontsize=12&15MinDelay=F&lang=1&titlestyle=1&vol=1&Indicator=3&indpara1=3&indpara2=6&indpara3=9&indpara4=12&indpara5=15&subChart2=3&ref2para1=12&ref2para2=26&ref2para3=9&subChart3=12&ref3para1=0&ref3para2=0&ref3para3=0&scheme=3&com=100&chartwidth=680&chartheight=400&stockid=";
   thisImgTail="&type=1&logoStyle=1' ";
 
 //  $( "#codelist" ).append("Total: " + theList.length + "<br>" + theList+ "<br>" );
@@ -31,6 +31,8 @@ function showAllCharts(){
     theFunccode =  "<div>" + (codeNo+1) + " &emsp; " + theText + "<br>" + thisImgHead + theText + thisImgPCode + thisImgTail + "onclick = \"xunbao('" + theText + "')\">" + "</div>";
     $( "#codelist" ).append( theFunccode);
   }
+ lazyLoadInstance.update();
+
 }
 
 function chkKey() { testkey = getChar(event);
@@ -99,5 +101,5 @@ function fullStkChartInit() {
 
 function xunbao(xunbaocode) {
   localStorage.setItem("randomcode", xunbaocode)
-  window.open("Random Charts.html");
+  window.open("D:/Dropbox/Public/LibDocs/Random Charts.html");
 }
