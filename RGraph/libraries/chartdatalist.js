83f4134535b03870877e5db99facce7dbee58cf5
williamkpchan <williamkpchan@gmail.com>
2020-08-29 10:20:29 +0800

    update

---
+++ RGraph/libraries/chartdatalist.js
@@ -0,0 +1,3 @@
+dataList = [495,518,421,441,597,594,476,491,672,597,998,309,336,926,595,69]
+
+dataLabelList = ["cX350Dn","cX350Up","cX40Dn","cX40Up","cX700Dn","cX700Up","cX80Dn","cX80Up","cXWAvDn","cXWAvUp","DnTrig","dnXAmt","gtpdAmt","UpTrig","upXAmt","VlAmt"]
