4e61fa40b7193520186476f02c64da53b39b208f
williamkpchan <williamkpchan@gmail.com>
2021-11-26 10:13:16 +0800

    update

--- LibDocs/readbook.js
+++ LibDocs/readbook.js
@@ -69,7 +69,7 @@ if (typeof markerName == 'undefined') {
 }
 
 function changeTopic() {
- if (topicpointer >= topicLength) { topicpointer = 0;}
+ if (topicpointer >= topicLength-1) { topicpointer = 0;}
  else if (topicpointer < 0) { topicpointer = topicLength-1;}
  else { topicpointer = topicpointer + 1;}
 console.log(topicpointer)
