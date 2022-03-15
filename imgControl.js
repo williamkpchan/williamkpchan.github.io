5fc45e1c6b577e6dc8a97b6e4691404e126934ae
williamkpchan <williamkpchan@gmail.com>
2022-03-15 14:31:55 +0800

    update

--- imgControl.js
+++ imgControl.js
@@ -1,11 +1,30 @@
 $("img").click(function() {
  $('img').css('max-height', $(window).height());
- $('img').css('width', 'auto');
  window.location = "#topic-" + topicpointer;
 })
 
 $("img").dblclick(function() {
- $('img').css('max-height', "100%");
+ $('img').css('max-height', '100%');
  window.location = "#topic-" + topicpointer;
 })
 
+function pageScroll() {
+    window.scrollBy(0,1);
+    scrolldelay = setTimeout(pageScroll,10);
+}
+
+function randomScroll(){
+  topicpointer = Math.floor(Math.random() * topicLength)
+  window.location = "#topic-" + topicpointer;
+  pageScroll();
+}
+
+window.addEventListener('click', function (evt) {
+    if (evt.detail === 3) {
+      var x = window.scrollX;
+      var y = window.scrollY;
+
+      $("body").toggleClass('stop-scrolling');
+      window.scrollTo(x, y);
+    }
+});
