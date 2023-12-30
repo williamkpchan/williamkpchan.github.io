96808757c7941dd7fc9e6e5c16143c5a52f721a1
williamkpchan <williamkpchan@gmail.com>
2023-12-30 19:36:41 +0800

    update

--- showTips.js
+++ showTips.js
@@ -63,7 +63,7 @@ function init_theRange(newRange) {
   tipsListIdx = allIdx.slice(0, selectRange); // select the leading items
 
   // shuffle the remaining pointers
-  if (noShuffle === false) {
+  if ((noShuffle === false)|(noShuffle === null)) {
     tipsListIdx = shuffle(tipsListIdx)
     topicpointer = Math.floor(Math.random() * (tipsListIdx.length -1))
   }
