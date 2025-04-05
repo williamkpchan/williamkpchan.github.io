e4ce543ec6704f51f737c029bdf35a41647f3a15
williamkpchan <williamkpchan@gmail.com>
2025-04-05 13:22:39 +0800

    update

---
+++ LibDocs/randoptionsArray.js
@@ -0,0 +1,9 @@
+const optionsArray = [
+'physicalTherapyTips','personalQuality','个人素养','考试','dosBatchQuestionList',
+'JavaScriptTips','pythonTips','economicsTips','RtipsList','proverbList','RNotes',
+'phpQuiz','w3PhpMySql','PowerShellTutorial','improveEnglish','inkscapeTips',
+'人情世故','品德教育','networkTips','MedicalTips','pictList','语文Tips','人物',
+'古文观止','中醫Notes','gittips','cssTips','记忆方法','nodejsServerTips',
+'HTMLNotesTips','MySQLTips','technoTips','managementTips','AiTips',
+]
+
