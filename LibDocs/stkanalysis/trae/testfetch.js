296af3aeddb6189097adf5c497d3006021d37c96
williamkpchan <williamkpchan@gmail.com>
2025-04-13 19:29:51 +0800

    update

--- LibDocs/stkanalysis/trae/testfetch.js
+++ LibDocs/stkanalysis/trae/testfetch.js
@@ -1,6 +1,6 @@
 let stkdata;
 let stockData;
-  theCode = '00388'
+  theCode = '01810'
   //theCode = $("#editor").text
   theurl = 'https://web.ifzq.gtimg.cn/appstock/app/hkfqkline/get?_var=kline_dayqfq&param=hk' + theCode + ',day,,,42,qfq';
 
@@ -16,9 +16,18 @@ fetch(theurl)
   .then(text => {
     const jsonStr = text.replace('kline_dayqfq=', '');
     stkdata = JSON.parse(jsonStr);
+console.log("jsonStr: " + jsonStr + "<br><br>")
+console.log("theCode: " + theCode + "<br><br>")
+console.log("Object.keys stkdata: "+Object.keys(stkdata))
+console.log("Object.keys stkdata.data: "+Object.keys(stkdata.data))
+console.log("Object.keys stkdata.data[`hk${theCode}`]: "+Object.keys(stkdata.data[`hk${theCode}`]))
+console.log("Object.keys stkdata.data[`hk${theCode}`].qt): "+Object.keys(stkdata.data[`hk${theCode}`].qt))
+console.log("Object.keys stkdata.data[`hk${theCode}`].qt[`hk${theCode}`]): "+Object.keys(stkdata.data[`hk${theCode}`].qt[`hk${theCode}`]))
+console.log("Object.keys stkdata.data[`hk${theCode}`].qt[`hk${theCode}`][1]): "+ stkdata.data[`hk${theCode}`].qt[`hk${theCode}`][1])
 
     if (stkdata.code === 0) {
-      console.log(Object.keys(stkdata.data[`hk${theCode}`]))
+      const stkNameObj = stkdata.data[`hk${theCode}`].qt
+
       const qtValue = stkdata.data[`hk${theCode}`].qt[`hk${theCode}`][44]
       console.log(qtValue);
       console.log(parseInt(qtValue));
