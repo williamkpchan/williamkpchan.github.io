77f57f1e120fde74117f59b3ea6de7f1b74fa4aa
williamkpchan <williamkpchan@gmail.com>
2025-06-18 18:09:38 +0800

    update

--- LibDocs/stkanalysis/trae/pinterest_scraper/filterpinterestfile.R
+++ LibDocs/stkanalysis/trae/pinterest_scraper/filterpinterestfile.R
@@ -3,26 +3,47 @@ library(crayon)
 workFolder = "D:/Dropbox/Public/LibDocs/stkanalysis/trae/pinterest_scraper"
 setwd(workFolder)
 #filename = "nameHeader_250615_1533"
-cat(red("remove empty lines!"))
+cat(red("\n\n\nremove empty lines!\n"))
+cat(pink("make sure all imgs on one line of its own! and remove empty lines\n\n\n"))
+
 
 filename = readline("enter filename without extension: ")
 wholePage = readLines(paste0(filename,".html"))
+wholePage = gsub("> .*", ">", wholePage)
+oldwholePageLen = length(wholePage)
 
 emptyLineIdx = grep("^\n", wholePage)
 if(length(emptyLineIdx)>0){
+  cat("\nemptyLine:", length(emptyLineIdx), "\n")
   wholePage = wholePage[-emptyLineIdx]
 }
 
 
 cat("all wholePage ",length(wholePage))
-cat(red("filtering..."))
+cat(red("\nfiltering...\n"))
+
 pngIdx = grep("\\.png", wholePage)
-wholePage = wholePage[-pngIdx]
+cat("\npngIdx ",  length(pngIdx),"\n")
+if(length(pngIdx)>0){
+  wholePage = wholePage[-pngIdx]
+}
+
+videoIdx = grep("videos", wholePage)
+cat("\nvideoIdx ",  length(videoIdx),"\n")
+if(length(videoIdx)>0){
+  wholePage = wholePage[-videoIdx]
+}
+
+smallImgIdx = grep("75x75", wholePage)
+cat("\nsmallImgIdx ",  length(smallImgIdx),"\n")
+if(length(smallImgIdx)>0){
+  wholePage = wholePage[-smallImgIdx]
+}
 
 uniqueElements = unique(gsub("<img.*/", "", wholePage))
 
 uniqueEleLen = length(uniqueElements)
-cat("uniqueElements ",uniqueEleLen)
+cat("\nuniqueElements length:",uniqueEleLen,"\n")
 
 removeIdx = numeric()
 
@@ -33,11 +54,20 @@ for(i in 1:uniqueEleLen){
 
   # seek elements
   eleIdx = grep(seekEle, wholePage)
+  cat(" common ele:", length(eleIdx),"")
+
   eleIdx = eleIdx[-length(eleIdx)]
+  cat("remove ele:", length(eleIdx),"")
 
   removeIdx = c(removeIdx, eleIdx)
+  removeIdx = unique(removeIdx)
+  cat("total remove elements:", length(removeIdx),"\n")
+
 }
 newwholePage = wholePage[-removeIdx]
+cat(red("remain elements:", length(newwholePage),"\n"))
+cat("\noriginal line number:",oldwholePageLen)
+cat("\nfinal line number:",length(newwholePage))
 
 newfilename = paste0(filename, "filtered.html")
 sink(newfilename)
